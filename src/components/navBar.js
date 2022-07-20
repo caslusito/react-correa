21e803f2c4d018e0545ebe00c2393b250dc3cb4e
Lucas Correa <lucasfran04@hotmail.com.ar>
2022-07-20 16:51:59 -0300

    segunda entrega

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,6 +1,16 @@
-function App () {
-    return <>
-    </>
+function App() {
+    return (
+        <header id="header">
+            <h1 className="tituloPrincipal">Kekeri Camisetas</h1>
+            <nav>
+                <ul className="navMenu">
+                    <li><a className="" href="#">HOME</a></li>
+                    <li><a className="" href="#">FAQ</a></li>
+                    <li><a className="" href="#">CONTACT</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
 };
 
 export default App;
